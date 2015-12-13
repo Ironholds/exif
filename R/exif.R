097ced6d81c2623eb3bafd27d11132e66f735545
ironholds <ironholds@gmail.com>
2015-12-12 21:57:28 -0500

    docs

--- R/exif.R
+++ R/exif.R
@@ -11,6 +11,19 @@
 NULL
 
 #'@title Read EXIF Metadata
+#'@description \code{read_exif} reads EXIF metadata from JPEG files,
+#'returning it as a data.frame.
+#'
+#'@param files a vector of files to read in.
+#'
+#'@return a data.frame, with each row consisting of the metadata for one file in \code{files}. Absent values are
+#'represented by an empty string for character columns, and 0 for numeric columns.
+#'
+#'@examples
+#'# A simple example using included images
+#'file <- system.file("extdata/dog_test_img.jpg", package="exif")
+#'file_metadata <- read_exif(file)
+#'
 #'@export
 read_exif <- function(files){
   return(read_exif_(normalizePath(files, winslash = "\\", mustWork = FALSE)))
